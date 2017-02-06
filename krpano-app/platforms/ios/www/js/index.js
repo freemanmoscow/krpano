b50b0e6e5b2fd347395e5e434021a9c923dd4aae
Pavel Usmanov <pavel.usmanov@gmail.com>
2017-02-05 21:22:41 -0500

    no message

--- krpano-app/platforms/ios/www/js/index.js
+++ krpano-app/platforms/ios/www/js/index.js
@@ -32,7 +32,7 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        window.location.href = 'http://krpano.paperplane.io';
+			window.location.href = 'http://krpano.paperplane.io';
     }
 };
 
